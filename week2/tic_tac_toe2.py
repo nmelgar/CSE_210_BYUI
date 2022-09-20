b77184716b4284e7c61f4cf86390ff6ce5754905
nmelgar <nmelgar16013@gmail.com>
2022-09-20 14:27:56 -0500

    Why it is returning none at the end when priting the list?

--- week2/tic_tac_toe2.py
+++ week2/tic_tac_toe2.py
@@ -5,40 +5,46 @@
 def main():
     rows = 3
     row_break = 1
-    list1 = get_list(rows)
-    # print(list1[2])
-    # grid(list1)
-    # print(list1)
-    # list1.pop(3)
-    # list1.insert(3, "X")
     index_counter = 0
-    for x in list1:
-        if x != 0:
-            print(x, end=' | ')
-        if list1[index_counter] == rows * row_break:
-            print("")
-            print("-----------")
-            row_break += 1
-        index_counter += 1
 
+    #
+    list1 = get_list(rows)
+    print(print_list(list1))
 
-# this will create the grid using the list of numbers
-# def grid(list1):
-    # print(list1)
-    # print(list1[2])
-    # list1.pop(2)
-    # list1.insert(2, "X")
-    # print(list1)
+    p1_choice = 0
+    p2_choice = 0
+
+    p1 = int(input("Choose a number: "))
+
+    if p1 in list1:
+        list1.pop(p1)
+        list1.insert(p1, "X")
+        p1_choice = p1
+    print(print_list(list1))
+
+    p2 = int(input("Choose a number: "))
+
+    if p2 in list1:
+        list1.pop(p2)
+        list1.insert(p2, "o")
+        p2_choice = p2
+
+    print(print_list(list1))
 
 
 # this will create the list of numbers
 def get_list(rows):
-
     rows_and_columns = rows * rows
     numbers = range(0, rows_and_columns + 1)
     list1 = list(numbers)
 
     return list1
+
+
+def print_list(list1):
+    for x in list1:
+        if x != 0:
+            print(x, end=' | ')
 
 
 if __name__ == "__main__":
