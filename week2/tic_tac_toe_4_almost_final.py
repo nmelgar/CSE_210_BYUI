c1685a2249f142a57007f80499d335d3e1db0bfc
nmelgar <nmelgar16013@gmail.com>
2022-09-23 12:51:14 -0500

    You can play and decide who wins :), maybe few details are missing.

--- week2/tic_tac_toe_4_almost_final.py
+++ week2/tic_tac_toe_4_almost_final.py
@@ -14,25 +14,38 @@ def main():
         8: "8",
         9: "9"
     }
+    # welcome the user
     print("-------------------------------")
     print("Welcome to the Tic-Tac-Toe game")
     print("-------------------------------")
     print("The first in connect 3 wins!---")
     print("-------------------------------")
 
+    # print the board of the game
     print_board(dict_1)
     print("")
     run = True
+
+    # run the game
     while run:
         player1_choice(dict_1)
         print_board(dict_1)
-        winner(dict_1)
+        if winner(dict_1) == False:
+            print("")
+            break
+
         player2_choice(dict_1)
         print_board(dict_1)
-        winner(dict_1)
+        if winner(dict_1) == False:
+            print("")
+            break
 
 
 def print_board(dict_1):
+    """This function will print the board for the players
+    Input: dictionary with the numbers from 1 to 9
+    Output: The 1-9 board ready to play
+    """
     print("")
     for key, value in dict_1.items():
         if key == 3 or key == 6:
@@ -44,55 +57,89 @@ def print_board(dict_1):
 
 
 def player1_choice(dict_1):
+    """This function will allow the player to choose an available number
+    Input: a valid integer
+    Output: will update the dictionary with an X at the selected key
+    """
     print("")
     choice = int(input("\nPlayer 1 Choose a number: "))
-    dict_1.update({choice: "X"})
+    if choice not in dict_1:
+        print("Please enter a valid number")
+        print("\nWe will pass to tht next player")
+    else:
+        dict_1.update({choice: "X"})
 
 
 def player2_choice(dict_1):
+    """This function will allow the player to choose an available number
+    Input: a valid integer
+    Output: will update the dictionary with an O at the selected key
+    """
     print("")
     choice = int(input("\nPlayer 2 Choose a number: "))
-    dict_1.update({choice: "O"})
+    if choice not in dict_1:
+        print("Please enter a valid number")
+        print("\nWe will pass to tht next player")
+    else:
+        dict_1.update({choice: "O"})
 
 
 def winner(dict_1):
+    """This function will decide if there is a winner
+    Input: the dictionary with the new values for the keys
+    """
     for key, value in dict_1.items():
-
         # for player 1 to win
-        if key == 1 and value == "X" and key == 2 and value == "X":
+        if dict_1[1] == "X" and dict_1[2] == "X" and dict_1[3] == "X":
             print("\nPlayer 1 wins!")
-        # elif (key == 1 and value == "X") and (key == 4 and value == "X") and (key == 7 and value == "X"):
-        #     print("\nPlayer 1 wins!")
-        # elif (key == 1 and value == "X") and (key == 5 and value == "X") and (key == 9 and value == "X"):
-        #     print("\nPlayer 1 wins!")
-        # elif (key == 2 and value == "X") and (key == 5 and value == "X") and (key == 8 and value == "X"):
-        #     print("\nPlayer 1 wins")
-        # elif (key == 3 and value == "X") and (key == 6 and value == "X") and (key == 9 and value == "X"):
-        #     print("\nPlayer 1 wins")
-        # elif (key == 3 and value == "X") and (key == 5 and value == "X") and (key == 7 and value == "X"):
-        #     print("\nPlayer 1 wins")
-        # elif (key == 4 and value == "X") and (key == 5 and value == "X") and (key == 6 and value == "X"):
-        #     print("\nPlayer 1 wins")
-        # elif (key == 7 and value == "X") and (key == 8 and value == "X") and (key == 9 and value == "X"):
-        #     print("\nPlayer 1 wins")
+            return False
+        elif dict_1[1] == "X" and dict_1[4] == "X" and dict_1[7] == "X":
+            print("\nPlayer 1 wins!")
+            return False
+        elif dict_1[1] == "X" and dict_1[5] == "X" and dict_1[9] == "X":
+            print("\nPlayer 1 wins!")
+            return False
+        elif dict_1[2] == "X" and dict_1[5] == "X" and dict_1[8] == "X":
+            print("\nPlayer 1 wins!")
+            return False
+        elif dict_1[3] == "X" and dict_1[6] == "X" and dict_1[9] == "X":
+            print("\nPlayer 1 wins!")
+            return False
+        elif dict_1[3] == "X" and dict_1[5] == "X" and dict_1[7] == "X":
+            print("\nPlayer 1 wins!")
+            return False
+        elif dict_1[4] == "X" and dict_1[5] == "X" and dict_1[6] == "X":
+            print("\nPlayer 1 wins!")
+            return False
+        elif dict_1[7] == "X" and dict_1[8] == "X" and dict_1[9] == "X":
+            print("\nPlayer 1 wins!")
+            return False
 
         # for player 2 to win
-        # elif (key == 1 and value == "O") and (key == 2 and value == "O") and (key == 3 and value == "O"):
-        #     print("\nPlayer 2 wins!")
-        # elif (key == 1 and value == "O") and (key == 4 and value == "O") and (key == 7 and value == "O"):
-        #     print("\nPlayer 2 wins!")
-        # elif (key == 1 and value == "O") and (key == 5 and value == "O") and (key == 9 and value == "O"):
-        #     print("\nPlayer 2 wins!")
-        # elif (key == 2 and value == "O") and (key == 5 and value == "O") and (key == 8 and value == "O"):
-        #     print("\nPlayer 2 wins!")
-        # elif (key == 3 and value == "O") and (key == 6 and value == "O") and (key == 9 and value == "O"):
-        #     print("\nPlayer 2 wins!")
-        # elif (key == 3 and value == "O") and (key == 5 and value == "O") and (key == 7 and value == "O"):
-        #     print("\nPlayer 2 wins!")
-        # elif (key == 4 and value == "O") and (key == 5 and value == "O") and (key == 6 and value == "O"):
-        #     print("\nPlayer 2 wins!")
-        # elif (key == 7 and value == "O") and (key == 8 and value == "O") and (key == 9 and value == "O"):
-        #     print("\nPlayer 2 wins!")
+        elif dict_1[1] == "O" and dict_1[2] == "O" and dict_1[3] == "O":
+            print("\nPlayer 2 wins!")
+            return False
+        elif dict_1[1] == "O" and dict_1[4] == "O" and dict_1[7] == "O":
+            print("\nPlayer 2 wins!")
+            return False
+        elif dict_1[1] == "O" and dict_1[5] == "O" and dict_1[9] == "O":
+            print("\nPlayer 2 wins!")
+            return False
+        elif dict_1[2] == "O" and dict_1[5] == "O" and dict_1[8] == "O":
+            print("\nPlayer 2 wins!")
+            return False
+        elif dict_1[3] == "O" and dict_1[6] == "O" and dict_1[9] == "O":
+            print("\nPlayer 2 wins!")
+            return False
+        elif dict_1[3] == "O" and dict_1[5] == "O" and dict_1[7] == "O":
+            print("\nPlayer 2 wins!")
+            return False
+        elif dict_1[4] == "O" and dict_1[5] == "O" and dict_1[6] == "O":
+            print("\nPlayer 2 wins!")
+            return False
+        elif dict_1[7] == "O" and dict_1[8] == "O" and dict_1[9] == "O":
+            print("\nPlayer 2 wins!")
+            return False
 
 
 if __name__ == "__main__":
