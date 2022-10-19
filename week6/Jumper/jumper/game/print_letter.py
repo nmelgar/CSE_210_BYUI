5ee31972b2c2023c33c5bfda9c26747dd7b5162d
nmelgar <nmelgar16013@gmail.com>
2022-10-19 09:37:25 -0500

    Replacing letters is working, missing duplicates.

--- week6/Jumper/jumper/game/print_letter.py
+++ week6/Jumper/jumper/game/print_letter.py
@@ -1,23 +1,51 @@
-def main():
-    guessed = False
-    word = "hello"
+word = "hello"
+word = list(word)
 
-    while guessed == False:
-        letter_input = input("Try a letter: ")
-        print(word)
-        print_word(word, letter_input)
-        
-def print_word(word, letter):
-    word = list(word)
-    for x in word:
-        if letter in word:
-            replace_value = word.index(letter)
-            word.insert(replace_value, letter)          
-            print(letter, end=" ")
-        else:
-            print("_", end=" ")
-    
+print("")
+run = True
+new_list = []
+
+for x in word:
+    new_list.append("_")
+
+while run:
+    # imprimir la lista de los underscores
+    for under in new_list:
+        print(under, end=" ")
+
+    letter = input("letter: ")
+
+    if letter in word:
+        replace_value = word.index(letter)
+        new_list.pop(replace_value)
+        new_list.insert(replace_value, letter)
+    else:
+        print("\nhi")
+
+    exit = input("Do you want to exit? ")
+    if exit == "s":
+        run = False
 
 
-if __name__ == "__main__":
-    main()
+# def main():
+#     guessed = False
+#     word = "hello"
+
+#     while guessed == False:
+#         letter_input = input("Try a letter: ")
+#         print(word)
+#         print_word(word, letter_input)
+
+# def print_word(word, letter):
+#     word = list(word)
+#     for x in word:
+#         if letter in word:
+#             replace_value = word.index(letter)
+#             word.insert(replace_value, letter)
+#             print(letter, end=" ")
+#         else:
+#             print("_", end=" ")
+
+
+# if __name__ == "__main__":
+#     main()
