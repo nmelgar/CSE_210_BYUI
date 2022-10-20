3d09d5e973a13e4732fd655dddc81179e38587dd
nmelgar <nmelgar16013@gmail.com>
2022-10-20 13:28:05 -0500

    Small changes to run the puzzle class correctly.

--- week6/Jumper/jumper/game/puzzle.py
+++ week6/Jumper/jumper/game/puzzle.py
@@ -14,16 +14,15 @@ class Puzzle:
         self.word = 0
 
     def list_of_words(self):
-        """This function will be to generate the list of word.
-        It will return the list of word so a random word can be generated.
+        """This function will be to generate the list of words.
+        It will return the list of words so a random word can be chosen later.
 
         Args:
             self (word): An instance of puzzle
         """
-
+        # use this line when not working at my local machine
+        # with open("words.txt", "r") as words:
         with open("/home/nmelgar/Projects/CSE_210_BYUI/cse210-01/week6/Jumper/jumper/trying/words.txt", "r") as words:
-            # use this line when not working at my local machine
-            # with open("words.txt", "r") as words:
             list_of_words = []
 
             for line in words:
@@ -33,6 +32,12 @@ class Puzzle:
             return list_of_words
 
     def random_number(self):
+        """This function will be to generate a random number.
+        This number will be used when generating the random word.
+
+        Args:
+            self (word): An instance of puzzle
+        """
         words = self.list_of_words()
         length = len(words)
         number = random.randint(0, length)
@@ -40,6 +45,13 @@ class Puzzle:
         return number
 
     def generate_word(self):
+        """This function will be to generate a random word. 
+        It will be taking the list_of_words() and random_number() functions,
+        not as parameters.
+
+        Args:
+            self (word): An instance of puzzle
+        """
         list_of_words = self.list_of_words()
         words = list(list_of_words)
         random_number = self.random_number()
