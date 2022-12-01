92b0feb62194355c28541aa7be3d9ea97f7275e0
nmelgar <nmelgar16013@gmail.com>
2022-12-01 08:38:12 -0600

    Added alternative backgrounds.

--- Space Invaders/space_invaders/settings.py
+++ Space Invaders/space_invaders/settings.py
@@ -9,4 +9,4 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
-        self.background_image = pygame.image.load("space_invaders/images/in_game_bg_alt.png")
+        self.background_image = pygame.image.load("space_invaders/images/in_game_bg_alt_3.png")
