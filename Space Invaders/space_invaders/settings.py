66491328db405c36bdfbec3cd205875967d4886b
nmelgar <nmelgar16013@gmail.com>
2022-12-02 08:28:15 -0600

    Trying different backgrounds for main screen.

--- Space Invaders/space_invaders/settings.py
+++ Space Invaders/space_invaders/settings.py
@@ -1,12 +1,15 @@
 import pygame
+
+
 class Settings:
     """a class to store the settings of the game"""
 
     def __init__(self):
         """Initialize the game's settings"""
-        
-        #screen settings
+
+        # screen settings
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
-        self.background_image = pygame.image.load("space_invaders/images/in_game_bg_alt_3.png")
+        self.background_image = pygame.image.load(
+            "space_invaders/images/in_game_bg_alt_3.png")
