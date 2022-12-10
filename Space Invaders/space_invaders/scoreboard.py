b2268463520b06af58c7a3761264aeeb609b36b4
nmelgar <nmelgar16013@gmail.com>
2022-12-10 17:14:03 -0600

    Small changes, but pushing before big changes to game.

--- Space Invaders/space_invaders/scoreboard.py
+++ Space Invaders/space_invaders/scoreboard.py
@@ -1,5 +1,6 @@
 import pygame.font
 
+
 class Scoreboard:
     """A class to report scoring information."""
 
@@ -15,28 +16,28 @@ class Scoreboard:
         # Prepare the initial score image.
         self.prep_score()
         self.prep_level()
-    
+
     def prep_score(self):
         """Turn the score into a rendered image."""
         rounded_score = round(self.stats.score, -1)
         score_str = f"{rounded_score:,}"
         self.score_image = self.font.render(score_str, True,
-        self.text_color, self.settings.bg_color)
+                                            self.text_color, self.settings.bg_color)
         # Display the score at the top right of the screen.
         self.score_rect = self.score_image.get_rect()
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
-    
+
     def show_score(self):
         """Draw score to the screen."""
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.level_image, self.level_rect)
-    
+
     def prep_level(self):
         """Turn the level into a rendered image."""
         level_str = str(self.stats.level)
         self.level_image = self.font.render(level_str, True,
-        self.text_color, self.settings.bg_color)
+                                            self.text_color, self.settings.bg_color)
         # Position the level below the score.
         self.level_rect = self.level_image.get_rect()
         self.level_rect.right = self.score_rect.right
